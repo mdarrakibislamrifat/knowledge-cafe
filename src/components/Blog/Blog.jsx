75900082562388ceddd8407444efbb08838b7772
unknown <rohanrifat43@gmail.com>
2023-09-11 23:14:48 +0600

    hey

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,14 +1,38 @@
 
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
-    console.log(blog)
+import { FaBookmark } from 'react-icons/fa';
+const Blog = ({blog,handleAddToBookmark}) => {
+    const {title,cover,author,author_img,posted_date,reading_time,hashtags}=blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between items-center mb-4'>
+                <div className='flex
+                '>
+                        <img className='w-14 h-14 rounded-full' src={author_img} alt="" />
+                        <div className='ml-6'>
+                            <h3 className='text-2xl'>{author}</h3>
+                            <p>{posted_date}</p>
+                        </div>
+                        
+                </div>
+                <div>
+                <span>{reading_time} min read</span>
+                <button onClick={()=>handleAddToBookmark(blog)}
+                className='ml-2 text-2xl text-red-600'><FaBookmark></FaBookmark></button>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash,idx)=><span key={idx}><a href=''>#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 Blog.propTypes={
-    blog:PropTypes.object.isRequired
+    blog:PropTypes.object.isRequired,
+    handleAddToBookmark:PropTypes.func
 }
 export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,9 @@
+
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
+import PropTypes from 'prop-types'
 
-
-const Blogs = () => {
+const Blogs = ({handleAddToBookmark}) => {
     const [blogs,setBlogs]=useState([])
     useEffect(()=>{
         fetch('blogs.json')
@@ -11,8 +13,13 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blog=><Blog handleAddToBookmark={handleAddToBookmark} key={blog.id} blog={blog}></Blog>)
+            }
         </div>
     );
 };
-
+Blogs.propsTypes={
+    handleAddToBookmark:PropTypes.func
+}
 export default Blogs;
